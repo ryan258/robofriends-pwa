716d7235afb92bfb3d9d31292757f1e4808003fb
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-07-06 11:43:18 -0500

    Updates

---
+++ precache-manifest.2dc6acadf0864af976321359e6622262.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341b55b88a76d933389917f2fbc12c9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a76a39f328a407111839",
+    "url": "/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "f6431b505784603a5650",
+    "url": "/static/css/main.93bae99b.chunk.css"
+  },
+  {
+    "revision": "a76a39f328a407111839",
+    "url": "/static/js/2.c7b8b75f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c7b8b75f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6431b505784603a5650",
+    "url": "/static/js/main.fc7c83fd.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
